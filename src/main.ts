f4e53dae641e99fa86a734d3fda037f1c8c670bd
Alvaro <cberna.alvaro@gmail.com>
2026-02-20 18:29:08 -0300

    otro commit

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   const url_frontend = configService.get<string>('URL_FRONTEND');
 
   app.enableCors({
-    origin: [url_frontend!, 'http://localhost:3000'], 
+    origin: 'https://web-admin-panel-production-6f15.up.railway.app', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
